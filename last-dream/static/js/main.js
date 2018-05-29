1cc20a8f6fcef5f59745047fd0958a019037f3bd
Steven Huang <steven.charles.huang@gmail.com>
2018-05-29 01:22:36 -0400

    testing crafty with ws

--- last-dream/static/js/main.js
+++ last-dream/static/js/main.js
@@ -4,18 +4,18 @@ $( document ).ready(function() {
   // Moveable Div
   Crafty.init(800,600, document.getElementById('game'));
   // A blue block, controlled by arrow keys
-  var player = Crafty.e("2D, Canvas, Color, Fourway")
+  let player = Crafty.e("2D, Canvas, Color, Fourway")
       .attr({x:100, y:100, w:50, h:50})
       .color("blue")
       .fourway(500);
 
 
-  let playerPos = () => {
-    setInterval(() => {
-      player.pos
-      // console.log("main.js says player: ", player.pos())
-    }, 500);
-  }
+  // let playerPos = () => {
+  //   setInterval(() => {
+  //     return player.pos
+  //     // console.log("main.js says player: ", player.pos())
+  //   }, 500);
+  // }
 
   /* WS Integration Beg */
 
@@ -26,19 +26,31 @@ $( document ).ready(function() {
       $('#welcome').text('Hello ' + name);
       websocket.send(JSON.stringify({
           name: name,
-          message: $('#message').val()
+          message: $('#message').val(),
       }));
       $('#message').focus();
       $('#message').val('');
+      console.log('main.js: This is websocket: ', websocket)
       return false;
   });
+  websocket.send = () => {
+    setInterval(() => {
+      return (player.x, player.y);
+    }, 250)
+    console.log('player.pos:', player.x)
+  }
 
   websocket.onmessage = function(evt) {
-      $('#messages').append($('<li>').html(evt.data));
+    $('#messages').append($('<li>').html(evt.data));
+    $('#game').draw(evt.data);
+    console.log('main.js websocket.onmessage evt.data: ', evt.data)
   };
 
+
+
   websocket.onerror = function(evt) {
-      $('#messages').append($('<li>').text('<span style="color: red;">ERROR:</span> ' + evt.data));
+      $('#messages').append($('<li>')
+        .text('<span style="color: red;">ERROR:</span> ' + evt.data));
   };
 
   /* WS Integration End */
