34dc8ccb882ca2edb202b81b73f37d1a41d7570c
Steven Huang <steven.charles.huang@gmail.com>
2018-05-29 13:50:25 -0400

    last commit before new stack

--- last-dream/static/js/main.js
+++ last-dream/static/js/main.js
@@ -4,11 +4,45 @@ $( document ).ready(function() {
   // Moveable Div
   Crafty.init(800,600, document.getElementById('game'));
   // A blue block, controlled by arrow keys
-  let player = Crafty.e("2D, Canvas, Color, Fourway")
+  let player = Crafty.e("2D, Canvas, Color, Fourway, Motion")
       .attr({x:100, y:100, w:50, h:50})
       .color("blue")
-      .fourway(500);
+      .fourway(500)
+    //   .bind('KeyDown', function(e) {
+    // if(e.key == Crafty.keys['LEFT_ARROW']) {
+    //   // this.x=this.x-1;
+    //   console.log('key did a thing')
+    //   } else if (e.key == Crafty.keys['RIGHT_ARROW']) {
+    //   // this.x=this.x+1;
+    //   console.log('key did a thing')
+    //   } else if (e.key == Crafty.keys['UP_ARROW']) {
+    //   // this.y=this.y-1;
+    //   console.log('key did a thing')
+    //   } else if (e.key == Crafty.keys['DOWN_ARROW']) {
+    //   // this.y=this.y+1;
+    //   console.log('key did a thing')
+    // }
+  // });
 
+  let playerMotion = player.motionDelta()
+
+  player.onMouseDown = function(e) {
+    // console.log('main.js onMouseDown: You did a thing', playerMotion);
+    console.log(`main.js onMouseDown: You did a thing.
+      player x: ${player.x}, player y: ${player.y}`);
+  };
+  Crafty.addEvent(player, Crafty.stage.elem, "mousedown", player.onMouseDown);
+
+  player.onKeyDown = (e) => {
+    console.log('main.js onKeyUp: Did a thing');
+  }
+  Crafty.addEvent(player, Crafty.stage.elem, "keydown", player.onKeyDown);
+
+  // player.bind("NewDirection", function(){
+  //       // `this` refers to the entity
+  //       console.log('main.js: binding triggered')
+  //   })
+  // player.trigger("ChangeColor", {color:"blue"});
 
   // let playerPos = () => {
   //   setInterval(() => {
@@ -24,13 +58,17 @@ $( document ).ready(function() {
       name = $('#name').val() ? $('#name').val() : 'Anonymous';
       $('#name-div').hide();
       $('#welcome').text('Hello ' + name);
+      console.log('main.js new name', name);
+      /* Crafty Implementing - Beg */
+      // websocket.send()
+      /* Crafty Implementing - End */
       websocket.send(JSON.stringify({
           name: name,
           message: $('#message').val(),
       }));
-      $('#message').focus();
+      console.log('main.js websocket post send(JSON): ', websocket);
+      // $('#message').focus();
       $('#message').val('');
-      console.log('main.js: This is websocket: ', websocket)
       return false;
   });
   // websocket.send = () => {
@@ -40,17 +78,19 @@ $( document ).ready(function() {
   //   console.log('player.pos:', player.x)
   // }
 
-  websocket.onmessage = function(e) {
-    $('#messages').append($('<li>').html(e.data));
+  websocket.onmessage = function(evt) {
+    $('#messages').append($('<li>').html(evt.data));
+    $('#messages').append($('<li>').html('schmuck-test'));
     console.log('main.js websocket.onmessage: event triggered');
-    console.log('main.js websocket.onmessage evt.data: ', e.data);
+    console.log('main.js websocket.onmessage evt.data: ', evt.data);
   };
 
 
 
-  websocket.onerror = function(e) {
+  websocket.onerror = function(evt) {
       $('#messages').append($('<li>')
-        .text('<span style="color: red;">ERROR:</span> ' + e.data));
+        .text('<span style="color: red;">ERROR:</span> ' + evt.data));
+      console.log('main.js websocket.onmessage: event triggered');
   };
 
   /* WS Integration End */
