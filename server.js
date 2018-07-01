d738ceaedb65634f8b824c6114a272ceae57ca50
Steven Huang <steven.charles.huang@gmail.com>
2018-07-01 10:50:20 -0400

    local host serving pub-html successful

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+
+const logger = require('morgan');
 const http = require('http');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, './public')));
+app.use(logger('dev'));
 
 
 app.listen(PORT, () => {
