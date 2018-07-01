158cc830fb17d992f570baa2c3916566f8641b5b
Steven Huang <steven.charles.huang@gmail.com>
2018-07-01 10:12:24 -0400

    purge for refactor

--- last-dream/src/components/Game.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Game() {
-  return (
-    <div id="game-load">
-    </div>
-  );
-}
--- last-dream/static/js/outcome_screen.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-  // Import Tests
-  console.log('battle_screen.js: jQuery ready!');
-  console.log('battle_screen loaded')
-
-// Closes jQuery
-})
