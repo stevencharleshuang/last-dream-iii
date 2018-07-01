579e6d0694f3ffde116e7b5b555fe8526f9024d9
Steven Huang <steven.charles.huang@gmail.com>
2018-07-01 11:05:18 -0400

    socket server standing

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const app = express();
+const express             = require('express');
+const app                 = express();
+const path                = require('path');
+const http                = require('http');
+const server              = http.createServer(app);
+const socketIO            = require('socket.io');
+const io                  = socketIO(server);
+const PORT                = process.env.PORT || 3000;
 
-const logger = require('morgan');
-const http = require('http');
-const path = require('path');
-const PORT = process.env.PORT || 3000;
+const logger    = require('morgan');
 
 app.use(express.static(path.join(__dirname, './public')));
 app.use(logger('dev'));
 
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
   console.log(`Server up and listening on port: ${PORT}`)
 })
