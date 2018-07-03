bffc6433286eb6ca7682448bfcb8102dd3ed39ab
Steven Huang <steven.charles.huang@gmail.com>
2018-07-03 01:56:17 -0400

    client key events triggering

--- public/js/world-screen.js
+++ public/js/world-screen.js
@@ -10,8 +10,9 @@ $( document ).ready(() => {
     const bg = Crafty.e('2D, DOM, background')
     Crafty.sprite('../images/locke_map.png', { locke: [ 0, 0, 20, 30 ] });
 
+    // Init Client Avatar At Randomized Location
     function initClientPlayer(x, y) {
-      console.log('initClientPlayer() called')
+      console.log('initClientPlayer() fired');
       clientPlayer = Crafty.e('2D, DOM, Color, locke, Collision, Motion')
         .attr({ x: x, y: y })
         .color("blue")
@@ -21,7 +22,37 @@ $( document ).ready(() => {
           // this.stop();
           Crafty.enterScene('battle_screen');
       });
+      if (!!clientPlayer) {
+      clientPlayer.bind('KeyDown', function(e) {
+        // let playerPos = {gameState:'gameState', id: playerID, x: clientPlayer._x, y: clientPlayer._y};
+        // console.log('playerPos: ', playerPos);
+        if (e.key == Crafty.keys.W) { // W = Up
+          console.log('Player hit W');
+          socket.emit('clientMoveUp');
+        }
+        else if (e.key == Crafty.keys.A) { // A = Left
+          console.log('Player hit A');
+          socket.emit('clientMoveLeft');
+        }
+        else if (e.key == Crafty.keys.S) { // S = Down
+          console.log('Player hit S');
+          socket.emit('clientMoveDown');
+        }
+        else if (e.key == Crafty.keys.D) { // D = Right
+          console.log('Player hit D');
+          socket.emit('clientMoveRight')
+        }
+      });
     }
+    };
+
+
+
+    // // TESTING ONLY - TBR - Player Position Loggging
+    // clientPlayer.onKeyDown = function(e) {
+    //   console.log(`main.js onKeyDown: You did a thing.
+    //     player x: ${clientPlayer._x}, player y: ${clientPlayer._y}`);
+    // };
 
     socket.on('connect', () => {
       console.log(`<<< Client: Socket Connection Open! Client Id: ${socket.id}`);
@@ -32,7 +63,7 @@ $( document ).ready(() => {
       players.forEach((player) => {
         $('.players-list').append(`<li>${player.id}</li>`);
         initClientPlayer(player.x, player.y);
-      })
+      });
     });
     socket.emit('new player');
 
